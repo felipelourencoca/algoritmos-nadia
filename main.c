a97609861eb85d8e28056b256e15e0759b62c59a
CANNAROZZO LOURENÇO <felipe.lourenco01@fatec.com>
2025-11-27 20:05:21 -0300

    segundo commit

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
+#include "stdio.h";
+#include "stdlib.h";
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
+    int *p;
+    p = malloc(sizeof(int) * 4 );
+  
+    printf("%p %n", p , *p);
+    
     return 0;
 }
